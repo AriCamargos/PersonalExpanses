0db1a5b2e054755339501852b282b3e7764509e4
Ariana Camargos <arianacamargos16@gmail.com>
2022-01-27 17:12:14 -0300

    add

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -14,52 +14,56 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 20,
-          child: FittedBox(
-            //Ajuda a diminuir o tamanho do texto para que caiba dentro do espaço
-            child: Text('${value!.toStringAsFixed(2)}'),
-          ),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Container(
-          //Tamanho da barra superior
-          height: 60,
-          width: 10,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
-                  ),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(5),
-                ),
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        //Isso é uma função
+        return Column(
+          children: [
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                //Ajuda a diminuir o tamanho do texto para que caiba dentro do espaço
+                child: Text('${value!.toStringAsFixed(2)}'),
               ),
-              FractionallySizedBox(
-                heightFactor: percentage,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(5),
+            ),
+            SizedBox(height: constraints.maxHeight * 0.05),
+            Container(
+              //Tamanho da barra superior
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
                   ),
-                ),
+                  FractionallySizedBox(
+                    heightFactor: percentage,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Text(label!),
-      ],
+            ),
+            SizedBox(height: constraints.maxHeight * 0.05),
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(child: Text(label!)),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -97,22 +97,41 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-        actions: [
-          IconButton(
-            onPressed: () => _openTransectionFormModal(context),
-            icon: Icon(Icons.add),
-          ),
-        ],
+    final appBar = AppBar(
+      title: Text(
+        'Despesas Pessoais',
+        style: TextStyle(
+            fontSize:
+                20 /* *
+              MediaQuery.of(context)
+                  .textScaleFactor,  */ //Escala de responsividade no tamanho da fonte
+            ),
       ),
+      actions: [
+        IconButton(
+          onPressed: () => _openTransectionFormModal(context),
+          icon: Icon(Icons.add),
+        ),
+      ],
+    );
+    final availabelHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Chart(_recentTransactions),
-            TransactionList(_transactions, _removeTransaction),
+            Container(
+              height: availabelHeight * 0.25,
+              child: Chart(_recentTransactions),
+            ),
+            Container(
+              height: availabelHeight * 0.75,
+              child: TransactionList(_transactions, _removeTransaction),
+            ),
           ],
         ),
       ),
